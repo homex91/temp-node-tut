89eba30487e1ea09d101bfba7287d4c90e716539
Abayomi <ekzell91@gmail.com>
2021-05-15 15:26:00 +0100

    first commit

---
+++ 2-global.js
@@ -0,0 +1,9 @@
+// Globals - No WIndow!!!
+
+// __dirname - path to current directory
+// __filename - __filename
+// require -  function to use modukes (CommonJS)
+// module -  info about current module (file)
+// process -  info about env where the program is being executed
+
+console.log(__dirname);
